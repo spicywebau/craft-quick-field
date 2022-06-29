317ef83a4a7c7697b2d2d235cbbb148f451906a6
Thomas Templeton <thomas.templeton.dev@gmail.com>
2022-06-29 10:32:48 +1000

    Add default English translation file

---
+++ src/translations/en/quick-field.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    '\'{name}\' field saved.' => '\'{name}\' field saved.',
+    '\'{name}\' field deleted.' => '\'{name}\' field deleted.',
+    'An unknown error occurred.' => 'An unknown error occurred.',
+    'Are you sure you want to delete this field?' => 'Are you sure you want to delete this field?',
+    'Are you sure you want to delete this group and all its fields?' => 'Are you sure you want to delete this group and all its fields?',
+    'Cancel' => 'Cancel',
+    'Could not delete the group.' => 'Could not delete the group.',
+    'Could not load all resources.' => 'Could not load all resources.',
+    'Could not save the group:' => 'Could not save the group:',
+    'Delete' => 'Delete',
+    'Invalid field group: {groupName}' => 'Invalid field group: {groupName}',
+    'New Field' => 'New Field',
+    'New field created.' => 'New field created.',
+    'New Group' => 'New Group',
+    'Rename' => 'Rename',
+    'Save' => 'Save',
+    'Settings' => 'Settings',
+    'What do you want to name the group?' => 'What do you want to name the group?',
+];
